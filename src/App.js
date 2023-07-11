0eea30077bb0d7c6ea407813d6690abac2f3e3c6
Paul Fadayo <paulfadayo@yahoo.com>
2023-07-12 00:14:02 +0100

    render list items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Logo />
       <Form />
       <ParkingList />
@@ -21,12 +26,31 @@ function Form() {
 }
 
 function ParkingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {" "}
+        {item.quantity} {item.description}
+      </span>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X items on your list, and you already packed X(%)</em>
     </footer>
   );
