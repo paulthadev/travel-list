9037a54bb4a6ef2a87f4331026635e3f6bdb06c8
Paul Fadayo <paulfadayo@yahoo.com>
2023-07-15 11:31:52 +0100

    refactoring

---
+++ src/Component/Form.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item.."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>ADD</button>
+    </form>
+  );
+}
